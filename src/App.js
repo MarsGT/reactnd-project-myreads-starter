59ef8415d4ec38a89c1d4d8cf7afb3638e6882d9
Liang-hsiao <marsgt@qq.com>
2019-04-18 23:52:20 +0800

    style: App.js 缩进修正

--- src/App.js
+++ src/App.js
@@ -6,56 +6,56 @@ import * as BooksAPI from './BooksAPI'
 import './App.css'
 
 class BooksApp extends Component {
-  state = {
-    currentlyReading: [],
-    wantToRead: [],
-    read: []
-  }
+    state = {
+        currentlyReading: [],
+        wantToRead: [],
+        read: []
+    }
 
-  componentDidMount() {
-    BooksAPI.getAll().then(books => {
-      const currentlyReading =
-        books
-          .filter((book) => (book.shelf == 'currentlyReading'))
-          .map((book) => ({
-            "cover": book.imageLinks.thumbnail,
-            "title": book.title + (book.subtitle ? `: ${book.subtitle}` : ''),
-            "authors": book.authors.join(', ')
-          }))
-      const wantToRead =
-        books
-          .filter((book) => (book.shelf == 'wantToRead'))
-          .map((book) => ({
-            "cover": book.imageLinks.thumbnail,
-            "title": book.title + (book.subtitle ? `: ${book.subtitle}` : ''),
-            "authors": book.authors.join(', ')
-          }))
-      const read =
-        books
-          .filter((book) => (book.shelf == 'read'))
-          .map((book) => ({
-            "cover": book.imageLinks.thumbnail,
-            "title": book.title + (book.subtitle ? `: ${book.subtitle}` : ''),
-            "authors": book.authors.join(', ')
-          }))
-      this.setState({ currentlyReading, wantToRead, read })
-    })
-  }
+    componentDidMount() {
+        BooksAPI.getAll().then(books => {
+            const currentlyReading =
+                books
+                    .filter((book) => (book.shelf == 'currentlyReading'))
+                    .map((book) => ({
+                        "cover": book.imageLinks.thumbnail,
+                        "title": book.title + (book.subtitle ? `: ${book.subtitle}` : ''),
+                        "authors": book.authors.join(', ')
+                    }))
+            const wantToRead =
+                books
+                    .filter((book) => (book.shelf == 'wantToRead'))
+                    .map((book) => ({
+                        "cover": book.imageLinks.thumbnail,
+                        "title": book.title + (book.subtitle ? `: ${book.subtitle}` : ''),
+                        "authors": book.authors.join(', ')
+                    }))
+            const read =
+                books
+                    .filter((book) => (book.shelf == 'read'))
+                    .map((book) => ({
+                        "cover": book.imageLinks.thumbnail,
+                        "title": book.title + (book.subtitle ? `: ${book.subtitle}` : ''),
+                        "authors": book.authors.join(', ')
+                    }))
+            this.setState({ currentlyReading, wantToRead, read })
+        })
+    }
 
-  render() {
-    return (
-      <div className="app">
-        <Route exact path='/' render={() => (
-          <ListBooks
-            listBooks={this.state}
-          />
-        )} />
-        <Route exact path='/search' render={() => (
-          <SearchBooks />
-        )} />
-      </div>
-    )
-  }
+    render() {
+        return (
+            <div className="app">
+                <Route exact path='/' render={() => (
+                    <ListBooks
+                        listBooks={this.state}
+                    />
+                )} />
+                <Route exact path='/search' render={() => (
+                    <SearchBooks />
+                )} />
+            </div>
+        )
+    }
 }
 
 export default BooksApp
