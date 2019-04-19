0859c599113b956d6880eac11f27a7e755753194
Liang-hsiao <marsgt@qq.com>
2019-04-19 10:53:57 +0800

    feature: 增加 BookShelf 组件

--- src/ListBooks.js
+++ src/ListBooks.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import BookShelf from './BookShelf'
 import { Link } from 'react-router-dom'
 
 class ListBooks extends Component {
@@ -30,34 +31,8 @@ class ListBooks extends Component {
                 </div>
                 <div className="list-books-content">
                     <div>
-                        {shelfList.map((shelf, i) => (
-                            <div className="bookshelf">
-                                <h2 className="bookshelf-title">{shelf.show}</h2>
-                                <div className="bookshelf-books">
-                                    <ol className="books-grid">
-                                        {listBooks[shelf.id].map(book => (
-                                            <li>
-                                                <div className="book">
-                                                    <div className="book-top">
-                                                        <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.cover})` }}></div>
-                                                        <div className="book-shelf-changer">
-                                                            <select>
-                                                                <option value="move" disabled>Move to...</option>
-                                                                <option value="currentlyReading">Currently Reading</option>
-                                                                <option value="wantToRead">Want to Read</option>
-                                                                <option value="read">Read</option>
-                                                                <option value="none">None</option>
-                                                            </select>
-                                                        </div>
-                                                    </div>
-                                                    <div className="book-title">{book.title}</div>
-                                                    <div className="book-authors">{book.authors}</div>
-                                                </div>
-                                            </li>
-                                        ))}
-                                    </ol>
-                                </div>
-                            </div>
+                        {shelfList.map((shelf) => (
+                            <BookShelf shelf={shelf} books={listBooks} />
                         ))}
                     </div>
                 </div>
