432e7839e90682a22b254ccc291a27ba189663f2
Liang-hsiao <marsgt@qq.com>
2019-04-23 13:45:21 +0800

    fix: 放弃搜索框校验功能，并卸载 react-html5-form

--- src/SearchBooks.js
+++ src/SearchBooks.js
@@ -9,45 +9,42 @@ class SearchBooks extends Component {
         listBooks: []
     }
 
+    // 调用后台API搜索图书
     searchBooks = () => {
         const result = 20; // Result结果数量最大值
         const { query } = this.state
 
-        if (query.length === 0)
-            return
-
-        BooksAPI.search(query, result).then(books => {
-            const listBooks =
-                books
-                    .map((book) => ({
-                        "cover": book.imageLinks ? book.imageLinks.thumbnail : `https://books.google.com/googlebooks/images/no_cover_thumb.gif`,
-                        "title": book.title + (book.subtitle ? `: ${book.subtitle}` : ''),
-                        "authors": book.authors.join(', ')
-                    }))
-            this.setState({ listBooks })
-        })
+        if (query.length > 0) { // 请求校验
+            BooksAPI.search(query, result).then(books => {
+                if (books.length > 0) {
+                    const listBooks =
+                        books
+                            .map((book) => ({
+                                "cover": book.imageLinks ? book.imageLinks.thumbnail : `https://books.google.com/googlebooks/images/no_cover_thumb.gif`,
+                                "title": book.title + (book.subtitle ? `: ${book.subtitle}` : ''),
+                                "authors": book.authors.join(', ')
+                            }))
+                    this.setState({ listBooks })
+                }
+            })
+        }
     }
 
     // 过滤Enter提交搜索内容
-    handleKeyPress = (k) => {
-        if (k === 'Enter') {
+    handleKeyPress = (ev) => {
+        const key = ev.key
+        if (key === 'Enter') {
             this.searchBooks()
         }
     }
 
-    handleInvalid = (e) => {
-        console.log(e)
-        // if (this.state.textInput.validity.typeMismatch) {
-        //     this.state.textInput.setCustomValidity('!!!');
-        // } else {
-        //     this.state.textInput.setCustomValidity('');
-        // }
-    }
-
-    updateQuery = (query) => {
+    // 更新state中的query字段
+    updateQuery = (ev) => {
+        const query = ev.target.value
         this.setState({ query: query.trim() })
     }
 
+    // 清空state中的query字段
     clearQuery = () => {
         this.setState({ query: '' })
     }
@@ -64,10 +61,8 @@ class SearchBooks extends Component {
                             type='text'
                             value={query}
                             placeholder='Search by title or author'
-                            pattern='[A-Za-z]'
-                            onKeyPress={event => this.handleKeyPress(event.key)}
-                            onChange={event => this.updateQuery(event.target.value)}
-                            onInvalid={event => this.handleInvalid(event)}
+                            onKeyPress={this.handleKeyPress}
+                            onChange={this.updateQuery}
                         />
                     </div>
                 </div>
