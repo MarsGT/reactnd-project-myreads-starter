281cce642e73d26ce7dd6c8c8b9a382a6c0fa4bc
Liang-hsiao <marsgt@qq.com>
2019-04-23 22:24:37 +0800

    fix: 代码小优化

--- src/BookShelf.js
+++ src/BookShelf.js
@@ -1,23 +1,21 @@
-import React, { Component } from 'react'
+import React from 'react'
 import Book from './Book'
 
-class BookShelf extends Component {
-    render() {
-        const { shelf, books } = this.props
+function BookShelf(props) {
+    const { shelf, books } = props
 
-        return (
-            <div className="bookshelf" >
-                <h2 className="bookshelf-title">{shelf.show}</h2>
-                <div className="bookshelf-books">
-                    <ol className="books-grid">
-                        {books[shelf.id].map((book, key) => (
-                            <Book key={key} currentBook={book} />
-                        ))}
-                    </ol>
-                </div>
+    return (
+        <div className="bookshelf" >
+            <h2 className="bookshelf-title">{shelf.show}</h2>
+            <div className="bookshelf-books">
+                <ol className="books-grid">
+                    {books[shelf.id].map((book, key) => (
+                        <Book key={key} currentBook={book} />
+                    ))}
+                </ol>
             </div>
-        )
-    }
+        </div>
+    )
 }
 
 export default BookShelf
