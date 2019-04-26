160e482327d7c9c734be2f19a9f2ea0438824b8f
Liang-hsiao <marsgt@qq.com>
2019-04-26 14:53:01 +0800

    fix: 修正图书菜单默认值（增加非空判断及一个 None 选项）

--- src/BookShelfChanger.js
+++ src/BookShelfChanger.js
@@ -4,11 +4,12 @@ function BookShelfChanger(props) {
     const { bookID, curShelf } = props
     return (
         <div className='book-shelf-changer'>
-            <select value={curShelf} onChange={ev => props.onMoveBook(ev, bookID)}>
+            <select value={curShelf ? curShelf : 'none'} onChange={ev => props.onMoveBook(ev, bookID)}>
                 <optgroup label='Move to...'>
                     <option value='currentlyReading'>Currently Reading</option>
                     <option value='wantToRead'>Want to Read</option>
                     <option value='read'>Read</option>
+                    <option value='none'>None</option>
                 </optgroup>
             </select>
         </div>
